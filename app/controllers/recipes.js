4f41a0f05a5e26eeb23b3e305caa45ef1d2b6cac
Bradley Leftley <bradleyleftley@192.168.0.100>
2021-03-28 22:16:24 +0100

    Add text search

--- app/controllers/recipes.js
+++ app/controllers/recipes.js
@@ -1,4 +1,10 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default class RecipesController extends Controller {
+    value = ''
+    @computed('value')
+    get data() {
+        return this.value === '' ? this.model : this.model.filter(recipe => recipe.name.includes(this.value));
+    }
 }
